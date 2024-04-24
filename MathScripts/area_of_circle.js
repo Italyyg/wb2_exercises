d076f27f14684d3119099ccef3ea3fd3bd252f71
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-04-24 12:12:30 -0500

    added circle area file

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,7 @@
+//variables create as input for calculation
+let radius = 3;
+let pi = 3.14;
+
+let area = pi * (radius**2)
+
+console.log("The area of a circle with radius " + radius + " is " + area)
