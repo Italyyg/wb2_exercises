dc5c6c9f28b959fb49ec5f1540581888e742f9eb
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-04-24 10:55:22 -0500

    completed tip.js

---
+++ tips.js
@@ -0,0 +1,10 @@
+//variables create as input for calculation
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+//do our calculations for variables above
+let totalDue = foodCost + tax + tip;
+
+//output the results to the console
+console.log ("The total due is " + totalDue);
